0fa460cf9e9a341b7317806b121b6ec43f0e0b43
Karatuğ Ozan Bircan <karatugo@gmail.com>
2020-03-01 22:40:10 +0300

    Implement form and file upload (#4)
    * Implement form and file upload
    
    * Add sample pdb and fa files
    
    * Add fasta_dict.py for calculation
    
    * Add sdp dict calculation
    
    * Add sqlite db
    
    * Implement sample calculations without proper DB results
    
    * Add Heroku config

--- app.py
+++ app.py
@@ -1,14 +1,113 @@
+import flask
+import os
+from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+from src.spial import get_aa_conservation, get_sdp
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db = SQLAlchemy(app)
+
+load_dotenv()
+app.secret_key = os.getenv("SECRET_KEY")
+app.config['UPLOAD_FOLDER'] = "./uploaded-fa-files"
+app.config['PDB_UPLOAD_FOLDER'] = "./static/jsmol/data/"
+
+FA_EXTENSIONS = {"fa"}
+PDB_EXTENSIONS = {"pdb"}
+
 head_title = "Spial"
+
+class Calculation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    conservation_dict1 = db.Column(db.String(200), nullable=False)
+    conservation_dict2 = db.Column(db.String(200), nullable=False)
+    consensus_threshold = db.Column(db.DECIMAL, nullable=False)
+    specificty_threshold = db.Column(db.DECIMAL, nullable=False)
+    sdp_dict = db.Column(db.JSON, nullable=False)
+    pdb_filename = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return "<Calculation {}>".format(self.id)
 
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template("index.html", head_title=head_title)
+    pdb_filename = None
+    if 'pdb_filename' in flask.session:
+        pdb_filename = flask.session['pdb_filename']
+        sdp_dict = None
 
+        try:
+            engine = db.create_engine('sqlite:///test.db', {})
+            connection = engine.connect()
+            metadata = db.MetaData()
+            calc = db.Table('Calculation', 
+                            metadata, 
+                            autoload=True, 
+                            autoload_with=engine)
+            query = db.select([calc]).where(calc.columns.pdb_filename == pdb_filename)
+            ResultProxy = connection.execute(query)
+            ResultSet = ResultProxy.fetchall()
+            sdp_dict = ResultSet[-1]["sdp_dict"]
+        except Exception as e:
+            return f"There was an issue adding to the DB: {e}"
+
+    return render_template("index.html", 
+                           head_title=head_title,
+                           pdb_filename=pdb_filename,
+                           sdp_dict=sdp_dict)
+
+def is_fa_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in FA_EXTENSIONS
+
+def is_pdb_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in PDB_EXTENSIONS
+
+def save_file_to_fs(upload_folder, f):
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(upload_folder, filename))
+
+@app.route('/spial', methods=["POST"])
+def spial():
+    alignment_a = request.files["alignment_a"]
+    if alignment_a and is_fa_file(alignment_a.filename):
+        save_file_to_fs(app.config['UPLOAD_FOLDER'], alignment_a)
+
+    alignment_b = request.files["alignment_b"]
+    if alignment_b and is_fa_file(alignment_b.filename):
+        save_file_to_fs(app.config['UPLOAD_FOLDER'], alignment_b)
+    
+    pdb = request.files["pdb"]
+    if pdb and is_pdb_file(pdb.filename):
+        save_file_to_fs(app.config['PDB_UPLOAD_FOLDER'], pdb)
+    
+    consensus_threshold = request.form["consensus_threshold"]
+    specificty_threshold = request.form["specificty_threshold"]
+
+    conservation_dict1 = get_aa_conservation(alignment_a.filename)
+    conservation_dict2 = get_aa_conservation(alignment_b.filename)
+    sdp_dict = get_sdp(conservation_dict1,
+                       conservation_dict2,
+                       consensus_threshold,
+                       specificty_threshold)
+
+    new_calc = Calculation(conservation_dict1=str(conservation_dict1),
+                           conservation_dict2=str(conservation_dict2),
+                           consensus_threshold=consensus_threshold,
+                           specificty_threshold=specificty_threshold,
+                           sdp_dict=str(sdp_dict),
+                           pdb_filename=pdb.filename)
+    try:
+        db.session.add(new_calc)
+        db.session.commit()
+    except Exception as e:
+        return f"There was an issue adding your task: {e}"
+
+    flask.session["pdb_filename"] = pdb.filename
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
---
+++ src/spial.py
@@ -0,0 +1,86 @@
+import sys
+import os
+import json
+import re
+import operator
+from .fasta_dict import *
+
+base_path = "./uploaded-fa-files/"
+fasta_files_path = base_path 
+
+def get_aa_conservation(fasta_file):
+    aa_count_dict = {}
+    fasta_dict = get_fasta_dict(base_path,fasta_file)
+    length = len(fasta_dict[list(fasta_dict.keys())[0]])
+    #print(length)
+    i = 0
+    for pos in range(0,length):
+        aa_count_dict[i] = {}
+        for k,v in fasta_dict.items():
+            if not aa_count_dict:
+                aa_count_dict[i].update({v[pos]:0})
+            if v[pos] not in aa_count_dict[i].keys():
+                #print(v[pos])
+                aa_count_dict[i].update({v[pos]:0})
+                #print(aa_count_dict)
+            if v[pos] in  aa_count_dict[i].keys():
+                aa_count_dict[i][v[pos]] += 1
+              
+        #print(i)
+        i += 1
+    #print(aa_count_dict)
+    conservation_dict = {}
+    for position, counts in aa_count_dict.items():
+        conservation_dict[position] = {}
+        raw_length = float(len(fasta_dict.keys()))
+        max_score_aa = max(aa_count_dict[position].items(), key=operator.itemgetter(1))[0]
+        max_score = float(aa_count_dict[position][max_score_aa]/raw_length)
+        conservation_dict[position].update({max_score_aa:max_score})
+    #print(conservation_dict)
+    return conservation_dict
+
+def get_sdp(conservation_dict1,conservation_dict2,consensus_treshold,specificity_treshold):
+    sdp_dict = {}
+    consensus_treshold = float(consensus_treshold)
+    specificity_treshold = float(specificity_treshold)
+    #print(len(list(conservation_dict1.keys())))
+    for position in range(0,len(list(conservation_dict1.keys()))):
+        sdp_dict[str(position)] = {}
+        aa1 = list(conservation_dict1[position].keys())[0]
+        aa2 = list(conservation_dict2[position].keys())[0]
+        score1 = conservation_dict1[position][aa1]
+        score2 = conservation_dict2[position][aa2]
+       
+        if aa1 == aa2:
+            if score1 > consensus_treshold and score2 > consensus_treshold:
+                sdp_dict[str(position)] = {aa1:{'score':score1,'type':'consensus'}}
+            elif score1 < consensus_treshold and score2 < consensus_treshold:
+                sdp_dict[str(position)] = {aa1:{'score':score1,'type':'non-specific'}}
+            elif score1 > consensus_treshold and score2 < consensus_treshold:
+                if score1 > specificity_treshold:
+                    sdp_dict[str(position)] = {aa1:{'score':score1,'type':'specific'}}
+                else:
+                    sdp_dict[str(position)] = {aa1:{'score':score1,'type':'non-specific'}}
+        elif aa1 != aa2:
+            if score1 > specificity_treshold and score2 > specificity_treshold:
+                sdp_dict[str(position)] = {aa1:{'score':score1,'type':'separately specific'}}
+            elif score1 > specificity_treshold and score2 < specificity_treshold:
+                sdp_dict[str(position)] = {aa1:{'score':score1,'type':'specific'}}    
+
+    return sdp_dict
+
+def dump_to_json(sdp_dict,json_file):
+    with open(base_path +json_file, 'w') as f:
+        json.dump(sdp_dict, f, ensure_ascii=False, indent=4)
+   
+
+
+if __name__ == "__main__":
+    fasta_file1= sys.argv[1] 
+    fasta_file2 = sys.argv[2] 
+    consensus_treshold = sys.argv[3]
+    specificity_treshold = sys.argv[4]
+    conservation_dict1 = get_aa_conservation(fasta_file1)
+    conservation_dict2 = get_aa_conservation(fasta_file2)
+    sdp_dict = get_sdp(conservation_dict1,conservation_dict2,consensus_treshold,specificity_treshold)
+    dump_to_json(sdp_dict,"sdp.json")
